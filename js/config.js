1ef10aa1e3c1c02d07adfbd15d20cf847c6fb704
nktkhonkaen09-bit <nkt.khonkaen09@gmail.com>
2026-08-20 08:39:20 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    API_URL:
+        "https://script.google.com/macros/s/AKfycbxCvk2wb6FydCKNkEDvApbTgZWmSFoAzPvvM2sSDOIwSgvftMoQxIX3cy7npP3_6xT4/exec",
+
+    SESSION_KEY: "student_session",
+    STUDENT_KEY: "student_data"
+};
