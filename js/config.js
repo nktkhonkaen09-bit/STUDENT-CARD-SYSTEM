51b4c735ce1f766ae573b1be0d3f37d470d476ed
nktkhonkaen09-bit <nkt.khonkaen09@gmail.com>
2026-08-20 09:30:45 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,12 @@
 const CONFIG = {
+
     API_URL:
         "https://script.google.com/macros/s/AKfycbxCvk2wb6FydCKNkEDvApbTgZWmSFoAzPvvM2sSDOIwSgvftMoQxIX3cy7npP3_6xT4/exec",
 
-    SESSION_KEY: "student_session",
-    STUDENT_KEY: "student_data"
+    SESSION_KEY:
+        "student_session",
+
+    STUDENT_KEY:
+        "student_data"
+
 };
